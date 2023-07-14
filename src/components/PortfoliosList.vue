5cfb967d051935a5fc0e554fb444161438bacd3e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-14 17:26:14 +0200

    add cursor ponter for paginator

--- src/components/PortfoliosList.vue
+++ src/components/PortfoliosList.vue
@@ -1,45 +1,55 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    data() {
-		return {
-			arrPortfolios: [],
-			currentPage: 1,
-			nPages: 0,
-		};
-	},
-    methods: {
-		changePage(page) {
-			this.currentPage = page;
-			this.getPortfolios();
-		},
-		getPortfolios() {
-			axios
-				.get('http://127.0.0.1:8000/api/portfolios', {
-					params: {
-						page: this.currentPage,
-					},
-				})
-				.then(response => {
-					this.arrPortfolios = response.data.data;
-					this.nPages = response.data.last_page;
-				});
-		},
-	},
-    created() {
-		// richiesta dati al server
-		axios
-			.get('http://127.0.0.1:8000/api/portfolios', {
-				params: {
-					page: this.currentPage,
-				},
-			})
-			.then(response => {
-				this.arrPortfolios = response.data.data;
-				this.nPages = response.data.last_page;
-			});
-	},
+  data() {
+    return {
+      arrPortfolios: [],
+      currentPage: 1,
+      nPages: 0,
+    };
+  },
+  methods: {
+    changePage(page) {
+      if (page === "next") {
+        if (this.currentPage < this.nPages) {
+          this.currentPage++;
+        }
+      } else if (page === "previous") {
+        if (this.currentPage > 1) {
+          this.currentPage--;
+        }
+      } else {
+        this.currentPage = page;
+      }
+      this.getPortfolios();
+    },
+    getPortfolios() {
+      axios
+        .get("http://127.0.0.1:8000/api/portfolios", {
+          params: {
+            page: this.currentPage,
+          },
+        })
+        .then((response) => {
+          this.arrPortfolios = response.data.data;
+          this.nPages = response.data.last_page;
+        });
+    },
+  },
+  created() {
+    // richiesta dati al server
+    axios
+      .get("http://127.0.0.1:8000/api/portfolios", {
+        params: {
+          page: this.currentPage,
+        },
+      })
+      .then((response) => {
+        this.arrPortfolios = response.data.data;
+        this.nPages = response.data.last_page;
+      });
+  },
 };
 </script>
 
@@ -47,15 +57,24 @@ export default {
   <h2>Questa è la lista dei Progetti.</h2>
 
   <div class="container d-flex gap-5 flex-wrap justify-content-center">
-    <div class="card p-3" style="width: 18rem" v-for="portfolio in arrPortfolios" :key="portfolio.id">
-      <img :src="portfolio.url_image" class="card-img-top" :alt="portfolio.name" />
+    <div
+      class="card p-3"
+      style="width: 18rem"
+      v-for="portfolio in arrPortfolios"
+      :key="portfolio.id"
+    >
+      <img
+        :src="portfolio.url_image"
+        class="card-img-top"
+        :alt="portfolio.name"
+      />
       <div class="card-body">
-        <h5 class="card-title">{{portfolio.name}}</h5>
+        <h5 class="card-title">{{ portfolio.name }}</h5>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">{{portfolio.client_name}}</li>
-        <li class="list-group-item">{{portfolio.pickup_date}}</li>
-        <li class="list-group-item">{{portfolio.deploy_date}}</li>
+        <li class="list-group-item">{{ portfolio.client_name }}</li>
+        <li class="list-group-item">{{ portfolio.pickup_date }}</li>
+        <li class="list-group-item">{{ portfolio.deploy_date }}</li>
       </ul>
       <div class="card-body">
         <a href="#" class="card-link">Card link</a>
@@ -63,6 +82,35 @@ export default {
       </div>
     </div>
   </div>
+
+  <div class="container my-4">
+    <nav>
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" @click="changePage('previous')">Previous</a>
+        </li>
+
+        <li
+          v-for="page in nPages"
+          :key="page"
+          class="page-item"
+          :class="{ active: page == currentPage }"
+        >
+          <a class="page-link" @click="changePage(page)">
+            {{ page }}
+          </a>
+        </li>
+
+        <li class="page-item">
+          <a class="page-link" @click="changePage('next')">Next</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+  cursor: pointer;
+}
+</style>
