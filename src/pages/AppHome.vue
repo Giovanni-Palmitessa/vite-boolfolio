3be1758ceebbefec98a261d3c269f4fae6bb4483
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-18 16:44:04 +0200

    add basic structure for apphome

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,9 +1,38 @@
 <script>
+import axios from "axios";
+import { store } from "../store";
 export default {};
 </script>
 
 <template>
-  <h2>Home page</h2>
+  <div class="row row-cols-3 g-0">
+    <div class="col" v-for="portfolio in arrPortfolios" :key="portfolio.id">
+      <router-link
+        class="d-block w-100 h-100"
+        :to="{ name: 'portfolios.show', params: { id: portfolio.id } }"
+      >
+        <img
+          class="w-100 h-100"
+          :src="store.getImageUrl(portfolio.image)"
+          :alt="portfolio.title"
+        />
+      </router-link>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+/* TODO: fix style of grid (center and fill the screen with 100vh) */
+
+.row {
+  height: 70vh;
+}
+
+.col {
+  height: calc(100% / 3);
+}
+
+img {
+  object-fit: cover;
+}
+</style>
