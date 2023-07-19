92ca9b864dbbdb8dc475356e99b0086dcd321a70
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-19 11:37:57 +0200

    add v model to inputs

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -5,6 +5,10 @@ export default {
   data() {
     return {
       store,
+      name: "",
+      email: "",
+      message: "",
+      newsletter: true,
     };
   },
   methods: {
@@ -25,6 +29,7 @@ export default {
         class="form-control"
         id="email"
         aria-describedby="emailHelp"
+        v-model="email"
       />
       <div id="emailHelp" class="form-text">
         We'll never share your email with anyone else.
@@ -33,17 +38,29 @@ export default {
 
     <div class="mb-3">
       <label for="name" class="form-label">Name</label>
-      <input type="text" class="form-control" id="name" />
+      <input type="text" class="form-control" id="name" v-model="name" />
     </div>
 
     <div class="mb-3">
       <label for="message" class="form-label">Messaggio</label>
-      <textarea class="form-control" id="message" rows="3"></textarea>
+      <textarea
+        class="form-control"
+        id="message"
+        rows="3"
+        v-model="message"
+      ></textarea>
     </div>
 
     <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <input
+        type="checkbox"
+        class="form-check-input"
+        id="exampleCheck1"
+        v-model="newsletter"
+      />
+      <label class="form-check-label" for="exampleCheck1"
+        >Iscriviti ala newsletter</label
+      >
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
