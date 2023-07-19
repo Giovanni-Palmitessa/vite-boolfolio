3fc8af678b0b18cd5422b88c230287fc386da633
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-19 10:55:04 +0200

    add basic structure of form

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -6,21 +6,28 @@ export default {};
 
   <form>
     <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <label for="email" class="form-label">Email address</label>
       <input
         type="email"
         class="form-control"
-        id="exampleInputEmail1"
+        id="email"
         aria-describedby="emailHelp"
       />
       <div id="emailHelp" class="form-text">
         We'll never share your email with anyone else.
       </div>
     </div>
+
     <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" />
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="name" />
     </div>
+
+    <div class="mb-3">
+      <label for="message" class="form-label">Messaggio</label>
+      <textarea class="form-control" id="message" rows="3"></textarea>
+    </div>
+
     <div class="mb-3 form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1" />
       <label class="form-check-label" for="exampleCheck1">Check me out</label>
